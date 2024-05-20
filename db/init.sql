6b17e73c87b1b6360d42d0c64f8b71a0c6db089a
LyricalPHM <167819717+LyricalPHM@users.noreply.github.com>
2024-05-20 22:06:28 +0300

    Update init.sql

--- db/init.sql
+++ db/init.sql
@@ -1,19 +1,13 @@
-CREATE DATABASE my_database1;
-
-CREATE TABLE email (
-    id SERIAL PRIMARY KEY,
-    email VARCHAR(255) NOT NULL
-);
-
-CREATE TABLE phone (
-    id SERIAL PRIMARY KEY,
-    phone_number VARCHAR(20) NOT NULL
-);
-
+CREATE USER ${DB_REPL_USER} REPLICATION LOGIN PASSWORD '${DB_REPL_PASSWORD}';
 SELECT pg_create_physical_replication_slot('replication_slot');
-CREATE USER replicator WITH REPLICATION ENCRYPTED PASSWORD 'replicator_password';
-CREATE TABLE hba ( lines text ); 
-COPY hba FROM '/var/lib/postgresql/data/pg_hba.conf'; 
-INSERT INTO hba (lines) VALUES ('host replication all 0.0.0.0/0 md5'); 
-COPY hba TO '/var/lib/postgresql/data/pg_hba.conf'; 
-SELECT pg_reload_conf();
+
+GRANT CONNECT ON DATABASE ${DB_DATABASE} TO ${DB_REPL_USER};
+GRANT USAGE ON SCHEMA public TO ${DB_REPL_USER};
+GRANT SELECT ON ALL TABLES IN SCHEMA public TO ${DB_REPL_USER};
+
+--CREATE DATABASE ${DB_DATABASE};
+
+-- Создание таблиц, если они не существуют
+CREATE TABLE IF NOT EXISTS phone (id SERIAL PRIMARY KEY, phone_number VARCHAR(100) NOT NULL);
+
+CREATE TABLE IF NOT EXISTS email (id SERIAL PRIMARY KEY, email VARCHAR(100) NOT NULL);
